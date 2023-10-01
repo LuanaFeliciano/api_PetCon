43e6e5e4080b296976c3e13711d35f3ee0679f97
John Anderson Armeni <johnarmeni.ti@gmail.com>
2023-10-01 11:29:15 -0300

    adicionando consulta

---
+++ DDD.Domain/ClinicaContext/Consulta.cs
@@ -0,0 +1,17 @@
+﻿using DDD.Domain.SecretariaContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace DDD.Domain.ClinicaContext
+{
+    public class Consulta
+    {
+        public int IdConsulta { get; set; }
+        public Veterinario IdVeterinario { get; set; } 
+        public <IList<Veterinario>> Veterinarios { get; set; }
+    }
+}
